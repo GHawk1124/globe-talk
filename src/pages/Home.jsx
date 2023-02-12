4ff685d68782348c86e64147c4e6ece811974d18
Aryeman Singh <aryeman@outlook.com>
2023-02-12 07:57:39 -0500

    new commits

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,7 @@
 import React from 'react';
 
-import Header from '../partials/Header';
-import PageIllustration from '../partials/PageIllustration';
 import HeroHome from '../partials/HeroHome';
-import FeaturesBlocks from '../partials/FeaturesBlocks';
-import FeaturesZigZag from '../partials/FeaturesZigzag';
-import Testimonials from '../partials/Testimonials';
-import Newsletter from '../partials/Newsletter';
-import Banner from '../partials/Banner';
+
 
 function Home() {
   return (
